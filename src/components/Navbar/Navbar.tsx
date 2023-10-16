1191791557be9cff1b4a2959230b98296331fec5
Anton <antonpdecesare@gmail.com>
2023-10-16 14:00:52 -0400

    PR Changes4

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,18 +5,16 @@ import "./Navbar.css";
 function Navbar() {
   const navigate = useNavigate();
 
-  const navToBabyDashboard = () => {
-    navigate("/baby-dashboard");
-  };
-
-  const navToCodingDashboard = () => {
-    navigate("/coding-dashboard");
-  };
-
   return (
     <div className="navbarContainer">
-      <Button buttonText="Baby List" handleClick={navToBabyDashboard} />
-      <Button buttonText="Coding List" handleClick={navToCodingDashboard} />
+      <Button
+        buttonText="Baby List"
+        handleClick={() => navigate("/baby-dashboard")}
+      />
+      <Button
+        buttonText="Coding List"
+        handleClick={() => navigate("/coding-dashboard")}
+      />
     </div>
   );
 }
